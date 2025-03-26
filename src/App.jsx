ce01b663c23984eab693c5db111379e8e1eec0b1
MigFerro <migueloliveiraferro@gmail.com>
2025-03-26 11:38:57 +0000

    added prettier formatting

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ const App = () => {
   return (
     <div className="">
       <h1 className="mt-5 mb-10 ml-5">Typing Game</h1>
-      <Words
-        size="50"
-      />
+      <Words size="50" />
     </div>
   );
 };
